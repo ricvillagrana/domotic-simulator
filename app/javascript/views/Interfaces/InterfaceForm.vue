6fcaf1dc640948322adf21d07e528b18647ddfcf
Ricardo Villagrana <rvillagrana@protonmail.com>
2018-11-19 14:27:21 -0600

    fix: validate network

--- app/javascript/views/Interfaces/InterfaceForm.vue
+++ app/javascript/views/Interfaces/InterfaceForm.vue
@@ -13,11 +13,13 @@
         <textarea name="description" class="textarea" :class="{ 'error' : errors.description }" placeholder="Descripción" v-model="currentInterface.description"></textarea>
         <span class="input-error-message" v-show="errors.description">{{ errors.description }}</span>
 
-        <label for="description" class="label">Descripción:</label>
-        <select name="network_id" class="input bg-white" v-model="currentInterface.network_id">
+        <label for="description" class="label">Red:</label>
+        <select name="network_id" class="input bg-white" :class="{ 'error' : errors.network_id }" v-model="currentInterface.network_id">
+              <option value="0" disabled selected> - Selecciona una red - </option>
+
           <option v-for="(network, key) in networks" :key="`network-${key}`" :value="network.id">{{ network.name }}</option>
         </select>
-        <span class="input-error-message" v-show="errors.description">{{ errors.description }}</span>
+        <span class="input-error-message" v-show="errors.network_id">{{ errors.network_id }}</span>
 
       </div>
     </div>
@@ -41,7 +43,7 @@ export default {
     currentInterface: {
       name: '',
       description: '',
-      network_id: null
+      network_id: 0
     },
     errors: {
       name: null,
@@ -50,6 +52,9 @@ export default {
     saving: false,
     networks: []
   }),
+  beforeMount () {
+    this.fetchNetworks()
+  },
   methods: {
     handleSave: function () {
       this.saving = true
@@ -112,11 +117,13 @@ export default {
     invalid () {
       this.errors.name = null
       this.errors.description = null
+      this.errors.network_id = null
 
       if (this.currentInterface.name === '') this.errors.name = 'Nombre inválido, debe contener algo.'
+      if (this.currentInterface.network_id <= 0 || this.currentInterface.network_id === null) this.errors.network_id = 'Red inválida, selecciona una.'
       if (this.currentInterface.description.length < 30) this.errors.description = `Descripción inválida, debe contener al menos 30 caracteres, tú tienes ${this.currentInterface.description.length}.`
 
-      return this.errors.name || this.errors.description
+      return this.errors.name || this.errors.description || this.errors.network_id
     }
   },
   watch: {
@@ -132,7 +139,7 @@ export default {
         this.currentInterface = {
           name: '',
           description: '',
-          network_id: null
+          network_id: 0
         }
       }
     }
